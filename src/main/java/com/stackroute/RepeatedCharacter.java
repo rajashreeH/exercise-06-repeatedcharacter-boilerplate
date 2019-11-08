37963caea3fc3008151f325088d394e2aacf5c9a
ubuntu <ubuntu@ip-172-31-95-30.ec2.internal>
2019-11-08 04:45:44 +0000

    assignment submittion

--- src/main/java/com/stackroute/RepeatedCharacter.java
+++ src/main/java/com/stackroute/RepeatedCharacter.java
@@ -1,14 +1,53 @@
 package com.stackroute;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class RepeatedCharacter {
 
-   	//Do not print anything other than what's expected/asked in problem
-    public static void main(String[] args) {
-   	//Use Scanner to get input from console
+   	private static Scanner scan;
 
+	//Do not print anything other than what's expected/asked in problem
+    public static void main(String[] args) {
+   	scan = new Scanner(System.in);
+    String input=scan.nextLine();
+    Character repeated=findRepeatedCharacter(input);
+    System.out.println(repeated);
 	}
 
 	public static Character findRepeatedCharacter(String input) {
-		return '1';
+		
+		HashMap<Character, Integer> charCount=new HashMap<>();
+		if(input==null||input.isEmpty()) {
+			return '0';
+		}
+		char[] charArray=input.toCharArray();
+		
+		for (char c : charArray) {
+			if(charCount.containsKey(c)) {
+				charCount.put(c, charCount.get(c)+1);
+			}
+			else {
+				charCount.put(c, 1);
+			}
+		}
+		
+		int maxCount=0;
+		char maxChar = 0;
+		for (Map.Entry<Character, Integer> countCheck : charCount.entrySet()) {
+			if(countCheck.getValue()>maxCount) {
+				maxCount=countCheck.getValue();
+				maxChar=countCheck.getKey();
+			}
+			else if(countCheck.getValue()==maxCount) {
+				if(countCheck.getKey()<maxChar) {
+					maxCount=countCheck.getValue();
+					maxChar=countCheck.getKey();
+				}
+			}
+		}
+		return maxChar;
 	}
 }
